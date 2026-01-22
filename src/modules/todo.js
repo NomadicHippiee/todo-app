c056aaff362277a7409abe45271e3929eb0fc609
NomadicHippiee <84047650+NomadicHippiee@users.noreply.github.com>
2026-01-22 19:36:31 +0100

    Todo module

---
+++ src/modules/todo.js
@@ -0,0 +1,12 @@
+class Todo {
+  constructor(title, description, dueDate, priority, isComplete = false) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.isComplete = isComplete;
+    this.id = crypto.randomUUID();
+  }
+}
+
+export default Todo;
