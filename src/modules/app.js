e4e1b779a1f6a8ce4bcc3295b84658b2811bcb9a
NomadicHippiee <84047650+NomadicHippiee@users.noreply.github.com>
2026-01-23 18:50:52 +0100

    App module with methods

--- src/modules/app.js
+++ src/modules/app.js
@@ -4,7 +4,10 @@ import Project from "./project.js";
 class App {
   constructor() {
     this.projects = [];
-    this.createDefaultProject();
+    this.loadFromStorage();
+    if (this.projects.length === 0) {
+        this.createDefaultProject();
+    }
   }
 
   createDefaultProject() {
@@ -34,25 +37,93 @@ class App {
   addProject(name, startDate, deadline) {
     const newProject = new Project(name, [], startDate, deadline);
     this.projects.push(newProject);
+    this.saveToStorage();
     return newProject;
-
   }
 
   removeProject(projectId) {
-    const index = this.projects.findIndex(project => project.id === projectId);
+    const index = this.projects.findIndex(
+      (project) => project.id === projectId,
+    );
     if (index !== -1) {
-        this.projects.splice(index, 1);
+      this.projects.splice(index, 1);
+      this.saveToStorage()
     }
-
   }
 
   getProject(projectId) {
-    return this.projects.find(project => project.id === projectId);
+    return this.projects.find((project) => project.id === projectId);
+  }
+
+  getAllProjects() {
+    return this.projects;
+  }
+
+  addTodoToProject(projectId, title) {
+    const project = this.getProject(projectId);
+    if (!project) return null;
+
+    const newTodo = new Todo(title, "", null, "normal");
+    project.todoArr.push(newTodo);
+    this.saveToStorage()
+    return newTodo;
+  }
+
+  addTodoToDefaultProject(title) {
+    const defaultProject = this.projects[0];
+    return this.addTodoToProject(defaultProject.id, title);
+  }
+
+  removeTodoFromProject(projectId, todoId) {
+    const project = this.getProject(projectId);
+    if (!project) return;
+
+    const index = project.todoArr.findIndex(todo => todo.id === todoId);
+    if (index !== -1) {
+        project.todoArr.splice(index, 1);
+        this.saveToStorage();
+    }
+  }
+
+  getTodosFromProject(projectId) {
+    const project = this.getProject(projectId);
+    if (!project) return [];
+    return project.todoArr;
+  }
+
+  saveToStorage(){
+    const data = JSON.stringify(this.projects);
+    localStorage.setItem('projects', data);
 
   }
 
-  getAllProjects(){
-    return this.projects;
+  loadFromStorage(){
+    const data = localStorage.getItem('projects');
+    if (!data) return;
+
+    const parsedData = JSON.parse(data);
+
+    this.projects = parsedData.map(projectData => {
+        const reconstructedTodos = projectData.todoArr.map(todoData =>{
+            const todo = new Todo(
+                todoData.title,
+                todoData.description,
+                todoData.dueDate,
+                todoData.priority,
+                todoData.isComplete
+            );
+            todo.id = todoData.id;
+            return todo;
+        });
+        const project = new Project(
+            projectData.name,
+            reconstructedTodos,
+            projectData.startDate,
+            projectData.deadline
+        );
+        project.id = projectData.id;
+        return project;
+    });
 
   }
 }
