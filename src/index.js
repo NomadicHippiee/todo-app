ce3d5abe1a2126f84c868cc777ca920996ef27c3
NomadicHippiee <84047650+NomadicHippiee@users.noreply.github.com>
2026-03-06 13:44:24 +0100

    Render project card UI

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import './style.css';
 import './components/header.js';
 import header from './components/header.js';
+import projectList from './components/projectList.js';
 
 console.log("Webpack is alive!");
 
 document.body.appendChild(header());
+document.body.appendChild(projectList());
