96dd046d4fbad0f07b9436183508ab1fb8fce430
NomadicHippiee <84047650+NomadicHippiee@users.noreply.github.com>
2026-03-07 21:03:28 +0100

    Add todos view component

---
+++ src/components/todosView.js
@@ -0,0 +1,46 @@
+import app from "../modules/app.js";
+
+function todosView(projectId, onBack) {
+    const container = document.createElement("div");
+    const project = app.getProject(projectId);
+    const backbtn = document.createElement("button");
+
+    backbtn.addEventListener("click", ()=>{
+        onBack()
+    });
+
+    container.appendChild(backbtn);
+
+    const todoTitle = document.createElement("h2");
+    todoTitle.textContent = project.name; 
+
+    container.appendChild(todoTitle);
+
+    const todos = app.getTodosFromProject(projectId);
+
+    if (todos.length === 0) {
+        const emptyMsg = document.createElement("p");
+        emptyMsg.textContent = "No todos yet"
+        container.appendChild(emptyMsg);
+    } else {
+        todos.forEach((todo) => {
+            const todoCard = document.createElement("div");
+            todoCard.classList.add("todo-card");
+            const todoName = document.createElement("h3");
+            todoName.textContent = todo.title;
+            const deleteBtn = document.createElement("button");
+            deleteBtn.textContent = "Delete";
+
+            deleteBtn.addEventListener("click", () =>{
+                app.removeTodoFromProject(projectId, todo.id);
+                onBack();
+            });
+
+            todoCard.append(todoName, deleteBtn);
+            container.appendChild(todoCard);
+        });
+    }
+    return container;
+}
+
+export default todosView;
